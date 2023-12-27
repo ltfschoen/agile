1ca29c50429c6cb29911455b8aa293b1328f64e5
Luke Schoen <ltfschoen@gmail.com>
2023-12-27 13:11:20 +1100

    wip

---
+++ README.md
@@ -0,0 +1,4 @@
+references:
+- https://developer.apple.com/forums/thread/678243
+- https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-views-in-a-loop-using-foreach
+- https://developer.apple.com/forums/thread/704674
--- agile/ContentView.swift
+++ agile/ContentView.swift
@@ -5,15 +5,40 @@
 //  Created by luke on 27/12/2023.
 //
 
+import Foundation
 import SwiftUI
 
+class Model {
+    var items = ["Lasagna", "Fettuccini", "Spaghetti", "Avo toast", "Tortellini", "Pizza"]
+
+    init(items: [String]) {
+        if items.count > 0 {
+            self.items = items
+        }
+    }
+
+    func menuItems() {
+        for item1 in self.items {
+            print(item1)
+        }
+    }
+}
+
 struct ContentView: View {
+    @State var model1 = Model(items: [])
     var body: some View {
         VStack {
+            ForEach(model1.items, id: \.self) { item in
+                Text(item.capitalized)
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                model1.menuItems()
+            } label: {
+                Text("Print menu items to console!")
+            }.padding(.vertical, 10.0)
         }
         .padding()
     }
