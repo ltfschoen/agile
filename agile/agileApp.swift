9123ef13a9aa16e2704689a4bb272870dc99df2b
Luke Schoen <ltfschoen@gmail.com>
2023-12-27 12:12:59 +1100

    Initial Commit

---
+++ agile/agileApp.swift
@@ -0,0 +1,17 @@
+//
+//  agileApp.swift
+//  agile
+//
+//  Created by luke on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct agileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
