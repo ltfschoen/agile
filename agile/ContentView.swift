9c9225a0622cbedb76acca7abc6f24ec845143b9
Luke Schoen <ltfschoen@gmail.com>
2023-12-27 13:26:29 +1100

    wip

--- agile/ContentView.swift
+++ agile/ContentView.swift
@@ -8,10 +8,33 @@
 import Foundation
 import SwiftUI
 
-class Model {
-    var items = ["Lasagna", "Fettuccini", "Spaghetti", "Avo toast", "Tortellini", "Pizza"]
+struct Item: Identifiable {
+    var id: Int
+    var name1: String = ""
+    var name2: String = ""
+    var name3: String = ""
+}
 
-    init(items: [String]) {
+// struct
+var lasagna = Item(
+    id: 1,
+    name1: "Lasagna",
+    name2: "Italian"
+)
+
+// struct
+var avo_toast = Item(
+    id: 2,
+    name1: "Avo toast",
+    name2: "Australia"
+)
+
+class Model {
+    var items: [Item] = [lasagna, avo_toast]
+    
+//    var items: String = ["Lasagna", "Fettuccini", "Spaghetti", "Avo toast", "Tortellini", "Pizza"]
+
+    init(items: [Item]) {
         if items.count > 0 {
             self.items = items
         }
@@ -28,9 +51,14 @@ struct ContentView: View {
     @State var model1 = Model(items: [])
     var body: some View {
         VStack {
-            ForEach(model1.items, id: \.self) { item in
-                Text(item.capitalized)
+            // use if items is an array of structs
+            ForEach(model1.items) { item in
+                Text("Item: \(item.id), \(item.name1.capitalized), \(item.name2.capitalized)")
             }
+//            // use if items is an array of strings
+//            ForEach(model1.items, id: \.self) { item in
+//                Text(item.capitalized)
+//            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
